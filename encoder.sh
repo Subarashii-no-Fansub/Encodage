b804f97fae9e6a2bee668c55f5967608ba9e53f9
Subarashii no Fansub <subarashii-no-fansub@null.net>
2014-05-10 23:05:28 +0200

    Utilisation du -p pour le read

--- encoder.sh
+++ encoder.sh
@@ -11,22 +11,16 @@ file_end="$3.mkv"
 chmod 644 "$1"
 
 mkvmerge -i "$1"
-echo -n "Numéro de la vidéo : "
-read var_idvid
-echo -n "Numéro du son : "
-read var_idson
-echo -n "Numéro des sous-titres : "
-read var_idss
+read -p "Numéro de la vidéo : " var_idvid
+read -p "Numéro du son : " var_idson
+read -p "Numéro des sous-titres : " var_idss
 
 mkvmerge -o "$file"  --default-track $var_idvid:yes --forced-track $var_idvid:no --language $var_idson:jpn --default-track $var_idson:yes --forced-track $var_idson:no --audio-tracks $var_idson --video-tracks $var_idvid --no-subtitles --no-track-tags --no-global-tags "$1" --forced-track $var_idvid:no --subtitle-tracks $var_idvid --no-video --no-audio --no-track-tags --no-global-tags --no-chapters "$2"
 
 mkvmerge -i "$file"
-echo -n "Numéro de la vidéo : "
-read var_idvid
-echo -n "Numéro du son : "
-read var_idson
-echo -n "Numéro des sous-titres : "
-read var_idss
+read -p "Numéro de la vidéo : " var_idvid
+read -p "Numéro du son : " var_idson
+read -p "Numéro des sous-titres : " var_idss
 
 chmod 644 "$file"
 mkvmerge -o "$file_end" --default-track $var_idvid:yes --forced-track $var_idvid:no --language $var_idson:jpn --default-track $var_idson:yes --forced-track $var_idson:no --language $var_idss:fre --track-name $var_idss:"Subarashii no Fansub" --default-track $var_idss:yes --forced-track $var_idss:no --audio-tracks $var_idson --video-tracks $var_idvid --subtitle-tracks $var_idss  --no-track-tags --no-global-tags "$file"
