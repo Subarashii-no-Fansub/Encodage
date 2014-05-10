285870a8333d53364a77b22fc9b7cb544a18e343
Subarashii no Fansub <subarashii-no-fansub@null.net>
2014-05-10 16:52:04 +0200

    Change chmod and protect name from space

--- encoder.sh
+++ encoder.sh
@@ -5,15 +5,15 @@
 
 set -e
 
-file="$PWD/$3_first.mkv"
-file_end="$PWD/$3.mkv"
+file="$3_first.mkv"
+file_end="$3.mkv"
 
-chmod 777 $1
-mkvmerge -o $file  --default-track 0:yes --forced-track 0:no --language 1:jpn --default-track 1:yes --forced-track 1:no --audio-tracks 1 --video-tracks 0 --no-subtitles --no-track-tags --no-global-tags $1 --forced-track 0:no --subtitle-tracks 0 --no-video --no-audio --no-track-tags --no-global-tags --no-chapters $2
+chmod 644 "$1"
+mkvmerge -o "$file"  --default-track 0:yes --forced-track 0:no --language 1:jpn --default-track 1:yes --forced-track 1:no --audio-tracks 1 --video-tracks 0 --no-subtitles --no-track-tags --no-global-tags "$1" --forced-track 0:no --subtitle-tracks 0 --no-video --no-audio --no-track-tags --no-global-tags --no-chapters "$2"
 
-chmod 777 $file
-mkvmerge -o $file_end --default-track 0:yes --forced-track 0:no --language 1:jpn --default-track 1:yes --forced-track 1:no --language 2:fre --track-name 2:"Subarashii no Fansub" --default-track 2:yes --forced-track 2:no --audio-tracks 1 --video-tracks 0 --subtitle-tracks 2  --no-track-tags --no-global-tags "$file"
-chmod 777 $file_end
-rm $file
+chmod 644 "$file"
+mkvmerge -o "$file_end" --default-track 0:yes --forced-track 0:no --language 1:jpn --default-track 1:yes --forced-track 1:no --language 2:fre --track-name 2:"Subarashii no Fansub" --default-track 2:yes --forced-track 2:no --audio-tracks 1 --video-tracks 0 --subtitle-tracks 2  --no-track-tags --no-global-tags "$file"
+chmod 644 "$file_end"
+rm "$file"
 
-echo "Le CRC de ce fichier est : " && crc32 $file_end
+echo "Le CRC de ce fichier est : " && crc32 "$file_end"
