cf9087f7d45ac6efb47897e354f57caa650241bf
Subarashii no Fansub <Subarashii-no-Fansub@users.noreply.github.com>
2015-07-30 00:49:31 +0200

    Update changerVideo.sh

--- changerVideo.sh
+++ changerVideo.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # paramètre 1 : video $1
 # paramètre 2 : vidéo $2
 # paramètre 3 : titre $3
@@ -24,7 +24,5 @@ read -p "Numéro du son : " var_idson_deux
 read -p "Numéro des sous-titres : " var_idss_deux
 
 mkvmerge -o "$file" --default-track $var_idss_un:yes --forced-track $var_idss_un:no --subtitle-tracks $var_idss_un --no-video --no-audio --no-global-tags --no-chapters "$1" --forced-track $var_idvid_deux:no --forced-track $var_idson_deux:no --audio-tracks $var_idson_deux --video-tracks $var_idvid_deux --no-attachments --no-subtitles --no-track-tags -no-global-tags "$2"
-
-
 
 echo "FINI !"
